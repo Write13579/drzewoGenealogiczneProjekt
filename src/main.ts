ba3334ca31f07bb88ee8505566a7c899f8db41b2
Patryk Baraniak <pb311239@student.polsl.pl>
2024-02-23 07:24:19 +0100

    submit/edit ale usuwa za duzo

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,19 @@ let DATA_BASE: { firstName: string; lastName: string; age: number }[] = [];
 function add() {
   const mainElements = document.getElementById(`alles`)!;
 
-  const SETUP = `<div class="person">
+  const newPerson = `<div class="person">
   <label>Photo</label>
   <input type="image" /><br />
-  <label>First Name</label>
+  <label>First Name:</label>
   <input type="text" id="fName" /><br />
-  <label>Last Name</label>
+  <label>Last Name:</label>
   <input type="text" id="lName" /><br />
-  <label>Age</label>
+  <label>Age:</label>
   <input type="number" min="0" id="age" /><br />
   <button id="submit" onclick="submit()">submit</button>
 </div>`;
 
-  mainElements.innerHTML = mainElements.innerHTML + SETUP;
+  mainElements.innerHTML = mainElements.innerHTML + newPerson;
 }
 
 function submit() {
@@ -29,5 +29,29 @@ function submit() {
     age: parseInt(age.value),
   });
 
-  console.log(DATA_BASE);
+  update();
+}
+
+function update() {
+  const mainElements = document.getElementById(`alles`)!;
+  const acceptPerson = DATA_BASE.map(
+    (person, index) => `<div class="person">
+  <label>Photo</label>
+  <input type="image" /><br />
+  <label>First Name:</label>
+  <p id="fName">${
+    person.firstName === "" ? `<i>nothing typed</i>` : person.firstName
+  } </p><br />
+  <label>Last Name:</label>
+  <p id="lName">${
+    person.lastName === "" ? `<i>nothing typed</i>` : person.lastName
+  }</p><br />
+  <label>Age:</label>
+  <p id="age">${
+    isNaN(person.age) ? `<i>nothing typed</i>` : person.age
+  }</p><br />
+  <button id="edit" onclick="edit(${index})">edit</button>
+</div>`
+  ).join("");
+  mainElements.innerHTML = acceptPerson;
 }
